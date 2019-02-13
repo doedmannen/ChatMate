e1d00ceee9d9c5647a4eb5c2f8cb9c19df5822f5
Ted <ted.agner@hotmail.com>
2019-02-13 12:52:34 +0100

    Write text to javaFX gui

--- src/client/Controller.java
+++ src/client/Controller.java
@@ -1,5 +1,59 @@
 package client;
 
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
 public class Controller {
-    // Lista för meddelande som vi fått av serverjävel :)
+
+    @FXML
+    private TextField input_text;
+    @FXML
+    private TextArea output_text;
+
+    public void inputText() {
+        input_text.setOnAction(event -> startTask());
+    }
+
+    @FXML
+    public void startTask()
+    {
+        // Create a Runnable
+        Runnable task = () -> runTask();
+
+        // Run the task in a background thread
+        Thread backgroundThread = new Thread(task);
+        // Terminate the running thread if the application exits
+        backgroundThread.setDaemon(true);
+        // Start the thread
+        backgroundThread.start();
+    }
+    public void runTask()
+    {
+        for(int i = 1; i <= 10; i++)
+        {
+            try
+            {
+                // Get the Status
+                final String status = "Processing " + i + " of " + 10;
+
+                // Update the Label on the JavaFx Application Thread
+//                Platform.runLater(() -> statusLabel.setText(status));
+
+                output_text.appendText(status+"\n");
+
+                Thread.sleep(1000);
+            }
+            catch (InterruptedException e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
 }
--- src/client/Main.java
+++ src/client/Main.java
@@ -7,11 +7,17 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+    public static Stage primaryStage;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        Main.primaryStage = primaryStage;
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("GUI.fxml"));
+        Parent root = loader.load();
+        Controller controller = loader.getController();
+        primaryStage.setUserData(controller);
+
+        primaryStage.setTitle("Chatter Matter");
         primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
     }
