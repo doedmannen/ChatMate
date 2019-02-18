f8d049346192b44b63d6abbd06effc396b547560
Ted <ted.agner@hotmail.com>
2019-02-18 09:41:03 +0100

    Started with changing message to a builder class

--- src/models/Message.java
+++ src/models/Message.java
@@ -5,33 +5,62 @@ import java.util.UUID;
 
 public class Message implements Serializable, Sendable{
 
-   public UUID SENDER;
-   public UUID RECEIVER;
-   public String NICKNAME;
-   public MessageType TYPE;
-   public String TEXT_CONTENT;
-   public String CHANNEL;
+   public static class MessageBuilder {
 
-   public Message() {
-      this.SENDER = null;
-      this.RECEIVER = null;
-      this.NICKNAME = null;
-      this.TYPE = MessageType.CHANNEL_MESSAGE;
-      this.TEXT_CONTENT = null;
-      this.CHANNEL = null;
-   }
 
-   public Message(MessageType type) {
-      this.SENDER = null;
-      this.RECEIVER = null;
-      this.NICKNAME = null;
-      this.TYPE = type;
-      this.TEXT_CONTENT = null;
-      this.CHANNEL = null;
-   }
+      private UUID sender;
+      private UUID receiver;
+      private String nickname;
+      private MessageType type;
+      private String TextContent;
+      private String channel;
 
-   @Override
-   public String toString() {
-      return "I am a message that was sent as an object :)";
+      public MessageBuilder(MessageType type) {
+         this.type = type;
+      }
+
+      public MessageBuilder fromSender(UUID sender) {
+         this.sender = sender;
+         return this;
+      }
+
+      public MessageBuilder toReceiver(UUID receiver) {
+         this.receiver = receiver;
+         return this;
+      }
+
+      public MessageBuilder nickname(String nickname) {
+         this.nickname = nickname;
+         return this;
+      }
+
+      public MessageBuilder withContent(String content) {
+         this.TextContent = content;
+         return this;
+      }
+
+
+//      public Message() {
+//         this.SENDER = null;
+//         this.RECEIVER = null;
+//         this.NICKNAME = null;
+//         this.TYPE = MessageType.CHANNEL_MESSAGE;
+//         this.TEXT_CONTENT = null;
+//         this.CHANNEL = null;
+//      }
+//
+//      public Message(MessageType type) {
+//         this.SENDER = null;
+//         this.RECEIVER = null;
+//         this.NICKNAME = null;
+//         this.TYPE = type;
+//         this.TEXT_CONTENT = null;
+//         this.CHANNEL = null;
+//      }
+
+      @Override
+      public String toString() {
+         return "I am a message that was sent as an object :)";
+      }
    }
 }
