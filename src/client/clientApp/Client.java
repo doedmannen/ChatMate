cc4e20fea0f965ecd18d8bac034d7580bf902275
Ted <ted.agner@hotmail.com>
2019-02-21 14:17:00 +0100

    Refactored MessageInboxHandler and MessageCreator

--- src/client/clientApp/Client.java
+++ src/client/clientApp/Client.java
@@ -37,7 +37,7 @@ public class Client {
       channelMessages = new ConcurrentHashMap<>();
 
       try {
-         socket = new Socket("10.155.88.55", 54322);
+         socket = new Socket("localhost", 54322);
          isRunning = true;
          sender = new Sender(socket);
          reciever = new Receiver(socket);
--- src/client/clientApp/MessageInboxHandler.java
+++ src/client/clientApp/MessageInboxHandler.java
@@ -26,110 +26,87 @@ public class MessageInboxHandler {
     public void messageSwitch(Message message) {
         Platform.runLater(() -> {
             switch (message.TYPE) {
+                case WHISPER_MESSAGE:
                 case CHANNEL_MESSAGE:
-//               Client.getInstance().getChannelMessages().get(message.CHANNEL).add(message);
-               process_CHANNEL_MESSAGE(message);
-               break;
-            case JOIN_CHANNEL:
-                process_JOIN_MESSAGE(message);
-               Client.getInstance().channelList.get(message.CHANNEL).add(new User(message.NICKNAME, message.SENDER));
-               controller.refreshUserList();
-//                controller.getChatBox().getChildren().add(messageCreator.joinChannelMessage(message));
-               break;
-            case LEAVE_CHANNEL:
-                process_LEAVE_MESSAGE(message);
-               User u = Client.getInstance().channelList.get(message.CHANNEL)
-                       .stream()
-                       .filter(user -> user.getID() == message.SENDER)
-                       .toArray(User[]::new)[0];
-               Client.getInstance().channelList.get(message.CHANNEL).remove(u);
-               controller.refreshUserList();
-//                controller.getChatBox().getChildren().add(messageCreator.leaveChannelMessage(message));
-               break;
-            case DISCONNECT:
-                process_DISCONNECT_MESSAGE(message);
-               User disconnect = Client.getInstance().channelList.get(message.CHANNEL)
-                       .stream()
-                       .filter(user -> user.getID() == message.SENDER)
-                       .toArray(User[]::new)[0];
-               Client.getInstance().channelList.get(message.CHANNEL).remove(disconnect);
-               controller.refreshUserList();
-//                controller.getChatBox().getChildren().add(messageCreator.disconnectMessage(message));
-               break;
-            case NICKNAME_CHANGE:
-                process_NICKNAME_CHANGE(message);
-               if (message.SENDER == Client.getInstance().getThisUser().getID()) {
-                        Client.getInstance().getThisUser().setNickName(message.TEXT_CONTENT);
-                        Client.getInstance().changeTitle();
-                    }
-                    Client.getInstance().channelList.get(message.CHANNEL).forEach(user -> {
-                        if (user.getID() == message.SENDER) {
-                            user.setNickName(message.TEXT_CONTENT);
-                        }
-                    });
-//                controller.getChatBox().getChildren().add(messageCreator.nicknameMessage(message));
+                case ERROR:
+                case WARNING:
+                    printLabelOnClient(message);
+                    addMessageToList(message);
+                    break;
+                case JOIN_CHANNEL:
+                    addMessageToList(message);
+                    addUserToList(message);
+                    printLabelOnClient(message);
                     controller.refreshUserList();
                     break;
-            case WHISPER_MESSAGE:
-                controller.getChatBox().getChildren().add(messageCreator.whisperMessage(message));
-               break;
-            case CONNECT:
-               Client.getInstance().setThisUser(new User(message.NICKNAME, message.RECEIVER));
-               Main.primaryStage.setTitle("Chatter Matter - " + message.NICKNAME);
-               break;
-            case ERROR:
-               break;
-            case WARNING:
-//                controller.getChatBox().getChildren().add(messageCreator.warningMessage(message));
-               break;
-         }
-      });
-   }
+                case LEAVE_CHANNEL:
+                case DISCONNECT:
+                    removeUserFromList(message);
+                    addMessageToList(message);
+                    printLabelOnClient(message);
+                    controller.refreshUserList();
+                    break;
+                case NICKNAME_CHANGE:
+                    changeNickname(message);
+                    printLabelOnClient(message);
+                    addMessageToList(message);
+                    controller.refreshUserList();
+                    break;
+                case CONNECT:
+                    connect(message);
+                    break;
+            }
+        });
+    }
 
-   public void process_CHANNELL(Channel channel) {
-      ArrayList<Label> list = Client.getInstance().getChannelMessages().getOrDefault(channel.getName(), new ArrayList<>());
-      Client.getInstance().getChannelMessages().put(channel.getName(), list);
-      Platform.runLater(() -> {
-         controller.channels.add(channel);
-      });
-   }
-    public void process_NICKNAME_CHANGE(Message message) {
-        Label label = messageCreator.nicknameMessage(message);
-        Client.getInstance().getChannelMessages().get(message.CHANNEL).add(label);
+    public void printLabelOnClient(Message message) {
+        Label label = new MessageCreator().createLabel(message);
         if (message.CHANNEL.equals(Client.getInstance().getCurrentChannel())) {
             controller.getChatBox().getChildren().add(label);
         }
     }
-   public void process_CHANNEL_MESSAGE(Message message) {
-      Label label = messageCreator.channelMessage(message);
-      Client.getInstance().getChannelMessages().get(message.CHANNEL).add(label);
-      if (message.CHANNEL.equals(Client.getInstance().getCurrentChannel())) {
-         controller.getChatBox().getChildren().add(label);
-      }
-   }
-    public void process_JOIN_MESSAGE(Message message) {
-        Label label = messageCreator.joinChannelMessage(message);
+    public void addMessageToList(Message message) {
+        Label label = new MessageCreator().createLabel(message);
         Client.getInstance().getChannelMessages().get(message.CHANNEL).add(label);
-        if (message.CHANNEL.equals(Client.getInstance().getCurrentChannel())) {
-            controller.getChatBox().getChildren().add(label);
-        }
     }
-    public void process_LEAVE_MESSAGE(Message message) {
-        Label label = messageCreator.leaveChannelMessage(message);
-        Client.getInstance().getChannelMessages().get(message.CHANNEL).add(label);
-        if (message.CHANNEL.equals(Client.getInstance().getCurrentChannel())) {
-            controller.getChatBox().getChildren().add(label);
-        }
+
+    public void addChannel(Channel channel) {
+        ArrayList<Label> list = Client.getInstance().getChannelMessages().getOrDefault(channel.getName(), new ArrayList<>());
+        Client.getInstance().getChannelMessages().put(channel.getName(), list);
+        Platform.runLater(() -> controller.channels.add(channel));
     }
-    public void process_DISCONNECT_MESSAGE(Message message) {
-        Label label = messageCreator.disconnectMessage(message);
-        Client.getInstance().getChannelMessages().get(message.CHANNEL).add(label);
-        if (message.CHANNEL.equals(Client.getInstance().getCurrentChannel())) {
-            controller.getChatBox().getChildren().add(label);
+
+    public void changeNickname(Message message) {
+        if (message.SENDER == Client.getInstance().getThisUser().getID()) {
+            Client.getInstance().getThisUser().setNickName(message.TEXT_CONTENT);
+            Client.getInstance().changeTitle();
         }
+        Client.getInstance().channelList.get(message.CHANNEL).forEach(user -> {
+            if (user.getID() == message.SENDER) {
+                user.setNickName(message.TEXT_CONTENT);
+            }
+        });
     }
-   public void printUsers() {
-      Platform.runLater(() -> controller.printUsers());
-   }
+
+    public void connect(Message message){
+        Client.getInstance().setThisUser(new User(message.NICKNAME, message.RECEIVER));
+        Client.getInstance().changeTitle();
+    }
+
+    public void addUserToList(Message message) {
+        Client.getInstance().channelList.get(message.CHANNEL).add(new User(message.NICKNAME, message.SENDER));
+    }
+
+    public void removeUserFromList(Message message) {
+        User user = Client.getInstance().channelList.get(message.CHANNEL)
+                .stream()
+                .filter(u -> u.getID() == message.SENDER)
+                .toArray(User[]::new)[0];
+        Client.getInstance().channelList.get(message.CHANNEL).remove(user);
+    }
+
+    public void printUsers() {
+        Platform.runLater(() -> controller.printUsers());
+    }
 
 }
